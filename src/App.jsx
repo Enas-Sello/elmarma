c001a92c22658afeaab1f83d835926d6d0bcfd7e
enas sello <enas.awad169@gmail.com>
2023-03-04 19:58:14 +0200

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/css/bootstrap.rtl.min.css"
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import { Settings } from "./setting"
-import { Home } from "./pages/Home"
 import { BrowserRouter as Router, RouterProvider } from "react-router-dom"
 import { Routs } from "./pages/allRouts"
 function App() {
